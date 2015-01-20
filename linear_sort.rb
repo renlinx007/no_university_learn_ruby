b733fc0f43215a7806538ae59a235d17b9ef5d6c
renlinx007 <mail3renjith@gmail.com>
2015-01-21 00:40:09 +0530

    initialised array

---
+++ linear_sort.rb
@@ -0,0 +1,18 @@
+#!/bin/ruby
+
+# simple linear sort in ruby
+ 
+#  OS : Ubuntu linux 13.04 with oldreleases repo :) in i3 box  
+
+#  Testing : irb,ruby version 1.9 		     
+
+#  Shell : Bash,buffered by tmux					     
+
+#  Editor : vim		     
+						     		
+
+a = [3,4,2,10]
+
+
+
+
