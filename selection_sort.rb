532c1b01ceda811850ad987575e0dff558cac2a1
renlinx007 <mail3renjith@gmail.com>
2015-01-21 00:48:14 +0530

    added the loop for selection sort

---
+++ selection_sort.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+
+# simple linear sort in ruby
+ 
+#  OS : Ubuntu linux 13.04 with oldreleases repo :) in i3 box  
+
+#  testing : irb,ruby version 1.9 		     
+
+#  Shell : Bash,buffered by tmux					     
+
+#  Editor : vim		     
+						     		
+
+a = [3,4,2,10]
+
+a.each do |a_element|
+ puts a_element
+end 
+
+
